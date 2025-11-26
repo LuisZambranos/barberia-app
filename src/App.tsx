5141ba28ee9a0300a28e4aec370b5149fdb19021
LuisZambranos <luisf.zambrano0810@gmail.com>
2025-11-26 18:39:38 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-600 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        ¡Funciona con TypeScript!
+      </h1>
+    </div>
+  )
+}
+
+export default App
