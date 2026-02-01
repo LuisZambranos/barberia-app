5b84757b828b4a6c8d57193ea83c77731038258b
LuisZambranos <luisf.zambrano0810@gmail.com>
2026-02-01 15:09:47 -0300

    no se en que estado quedo

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Booking from "./pages/Booking";
-import Login from "./pages/Login"; // <--- 1. Importa el componente
-import { AuthProvider } from './context/AuthContext';
+
 
 function App() {
   return (
-    <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />}/>
             <Route path="book" element={<Booking />}/>
-            
-            {/* 2. Agrega la ruta para el login */}
-            <Route path="login" element={<Login />}/> 
 
           </Route>
         </Routes>
       </BrowserRouter>
-    </AuthProvider>
   )
 }
 
