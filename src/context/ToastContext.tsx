e766b2cabbefcf894d6c62eadada27961a9921b5
LuisZambranos <luisf.zambrano0810@gmail.com>
2026-03-31 20:00:27 -0300

    sonido arreglado

--- src/context/ToastContext.tsx
+++ src/context/ToastContext.tsx
@@ -15,7 +15,7 @@ interface ToastContextType {
 
 const ToastContext = createContext<ToastContextType | undefined>(undefined);
 
-const NOTIFICATION_SOUND = 'https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3';
+const NOTIFICATION_SOUND = "/notificacion.mp3";
 
 export const ToastProvider = ({ children }: { children: ReactNode }) => {
   const [activeToast, setActiveToast] = useState<{ message: string; type: ToastType } | null>(null);
